c8ae93cad8a7f7941f00747baac25a226380c929
yongdoc <47826077+yongdoc@users.noreply.github.com>
2019-04-05 22:09:44 +0700

    Add files via upload

---
+++ soal2/soal2jual.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <unistd.h>
+#include <pthread.h>
+#define PORT 7001
+
+int stok;
+void *status (void *arg){
+    while(1){
+        printf("stok barang : %d\n",stok);
+        sleep(5);
+    }
+}
+
+int main(int argc, char const *argv[]) {
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+      
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+      
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( PORT );
+      
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+
+    pthread_t tid;
+    key_t key = 1799;
+
+    pthread_create(&(tid),NULL,&status,NULL);
+    stok=shmget(key,sizeof(int),IPC_CREAT|0666);
+
+    while(1){
+        char buffer[1024] = {0};
+        int *val;
+        val = shmat(stok,NULL,0);
+        valread = read( new_socket , buffer, 1024);
+        if(!(strcmp(buffer,"tambah"))){
+            *val++;
+        }
+    }
+    return 0;
+}
